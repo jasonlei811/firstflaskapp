f0081603d29f9bcbcecae9b71dbd3cf18095690a
Jason Lei <Jasonlei811@gmail.com>
2020-09-15 16:44:09 -0700

    Pray this fixes something.

--- code/code/app.py
+++ code/code/app.py
@@ -10,9 +10,12 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL'), 'sqlite:///data.db'
+@app.route('/')
+def home():
+    return 'Hello World!'
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'jason'
+app.secret_key = 'bob'
 api = Api(app)  
 
 jwt = JWT(app, authenticate, identity) #/auth
@@ -35,4 +38,10 @@ api.add_resource(UserRegister, '/register')
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    app.run(port=5000, debug=True)
+    
+    if app.config['DEBUG']:
+        @app.before_first_request
+        def create_tables():
+            db.create_all()
+    
+    app.run(port=5000)
