d63e3848e610cc899cafd3e037a02c54524dbe6a
Jason Lei <Jasonlei811@gmail.com>
2020-09-15 16:55:24 -0700

    Added debug to app.config.

--- code/code/app.py
+++ code/code/app.py
@@ -10,10 +10,11 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
 @app.route('/')
 def home():
     return 'Hello World!'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'bob'
 api = Api(app)  
