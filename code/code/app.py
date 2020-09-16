62218a8d401c517dc591562841103da5650eab65
Jason Lei <Jasonlei811@gmail.com>
2020-09-15 17:00:32 -0700

    Copy pasted some code.

--- code/code/app.py
+++ code/code/app.py
@@ -10,36 +10,30 @@ from resources.item import Item, ItemList
 from resources.store import Store, StoreList
 
 app = Flask(__name__)
+
 app.config['DEBUG'] = True
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL','sqlite:///data.db')
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.secret_key = 'bob'
-api = Api(app)  
+app.secret_key = 'jose'
+api = Api(app)
 
-jwt = JWT(app, authenticate, identity) #/auth
-
-#Error codes
-#404 is for error not found
-#whats the most popular http status code -- 200
-#creating also has it's own status code -- 201
-#accepted code -- 202
-#400 is bad request
+jwt = JWT(app, authenticate, identity)  # /auth
 
 api.add_resource(Store, '/store/<string:name>')
-api.add_resource(Item, '/item/<string:name>') 
+api.add_resource(Item, '/item/<string:name>')
 api.add_resource(ItemList, '/items')
 api.add_resource(StoreList, '/stores')
 
 api.add_resource(UserRegister, '/register')
 
-
 if __name__ == '__main__':
     from db import db
     db.init_app(app)
-    
+
     if app.config['DEBUG']:
         @app.before_first_request
         def create_tables():
             db.create_all()
-    
+
     app.run(port=5000)
